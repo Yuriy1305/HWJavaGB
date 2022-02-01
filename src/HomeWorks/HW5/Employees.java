8cf337f1e5acae9fa93dfb035947bfaf10470568
YuriyL <yuriy1305@gmail.com>
2022-02-01 16:57:14 +0300

    Commit5+

--- src/HomeWorks/HW5/Employees.java
+++ src/HomeWorks/HW5/Employees.java
@@ -1,2 +1,59 @@
-package HomeWorks.HW5;public class Employees {
+package HomeWorks.HW5;
+
+// 4. Создать массив из 5 сотрудников.
+// Пример:
+//   Person[] persArray = new Person[5]; // Вначале объявляем массив объектов
+//   persArray[0] = new Person("Ivanov Ivan", "Engineer", "ivivan@mailbox.com", "892312312", 30000, 30); // потом для каждой ячейки массива задаем объект
+// 5. С помощью цикла вывести информацию только о сотрудниках старше 40 лет.
+
+public class Employees {
+
+    public static void main(String[] args) {
+// п.4 ДЗ:
+        Person[] Department = new Person[]{
+                new Person("Ivanov", "Ivan", "Ivanovich", "engineer",
+                        "ivanovII@mail.ru", "+7(123)456-78-90", 30000, 1992),
+                new Person("Petrov", "Petr", "Petrovich", "manager",
+                        "petrovPP@mail.ru", "+7(123)456-78-91", 40000, 1982),
+                new Person("Smirnov", "Smir", "Smirovich", "artist",
+                        "smirnovSS@mail.ru", "+7(123)456-78-92", 50000, 1972),
+                new Person("Zhukov", "Zhuk", "Zhukovich", "warehouse manager",
+                        "zhukovZZ@mail.ru", "+7(123)456-78-93", 60000, 1962),
+                new Person("Voynov", "Voyn", "Voynovich", "security",
+                        "voynovVV@mail.ru", "+7(123)456-78-90", 70000, 1952),
+        };
+// п.5 ДЗ:
+// без методов:
+//        int j = 1;
+//        for (int i = 0; i < Department.length; i++) {
+//            if (Department[i].getAge() > 40) {
+//                System.out.println((j++) + ". " + Department[i].getpersonInfo());
+//            }
+//        }
+// с методами:
+       getAllEmployees (Department);
+
+       getOldEmployees (Department, 40);
+    }
+
+     private static void getAllEmployees(Person[] Department) {
+         System.out.println("List of employees: ");
+        for (int i = 0; i < Department.length; i++) {
+           System.out.println((i+1) + ". " + Department[i].getpersonInfo());
+        }
+    }
+
+    private static void getOldEmployees(Person[] Department, int age) {
+        System.out.println("List of employees older "+age+" years: ");
+        int j = 1;
+        for (int i = 0; i < Department.length; i++) {
+            if (Department[i].getAge() > age) {
+                System.out.println((j++) + ". " + Department[i].getpersonInfo());
+            }
+        }
+    }
+
 }
+
+
+
--- src/HomeWorks/HW5/Person.java
+++ src/HomeWorks/HW5/Person.java
@@ -1,2 +1,57 @@
-package HomeWorks.HW5;public class Person {
+package HomeWorks.HW5;
+
+//        1. Создать класс "Сотрудник" с полями: ФИО, должность, email, телефон, зарплата, возраст.
+//        2. Конструктор класса должен заполнять эти поля при создании объекта.
+//        3. Внутри класса «Сотрудник» написать метод, который выводит информацию об объекте в консоль.
+
+import java.time.YearMonth;
+
+// п.1 ДЗ:
+public class Person {
+
+    private String surname;
+    private String name;
+    private String fatherName;
+    private String position;
+    private String email;
+    private String phoneNumber;
+    private int salary;
+    private int birthYear; // год рождения - возраст определяем разницей годов текущего и рождения
+
+// п.2 ДЗ:
+    public Person(
+            String surname,
+            String name,
+            String fatherName,
+            String position,
+            String email,
+            String phoneNumber,
+            int salary,
+            int birthYear)
+    {
+        this.surname = surname;
+        this.name = name;
+        this.fatherName = fatherName;
+        this.position = position;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.salary = salary;
+        this.birthYear=birthYear;
+    }
+
+//Getter Age
+    int getAge() {
+        return YearMonth.now().getYear()-birthYear;
+    }
+
+// п.3 ДЗ:
+String getpersonInfo() {
+        return  this.surname+" "+this.name+" "+this.fatherName+
+                ", position: "+this.position+
+                ", e-mail: "+this.email+
+                ", phone number: "+this.phoneNumber+
+                ", salary: "+this.salary+"₽"+
+                ", age: "+getAge()+" years old.";
+    }
+
 }
