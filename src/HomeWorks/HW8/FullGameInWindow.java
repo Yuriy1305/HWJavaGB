9dc60acd9700f6db277b6842e4fcdf29c682bad9
YuriyL <yuriy1305@gmail.com>
2022-02-10 19:58:38 +0300

    Commit8

---
+++ src/HomeWorks/HW8/FullGameInWindow.java
@@ -0,0 +1,11 @@
+package HomeWorks.HW8;
+/**
+ * Created by Aleksandr Gladkov [Anticisco]
+ * Date: 03.02.2022
+ * Subsequent changes when doing homework by Yuriy Lomtev 10/02/2022
+ */
+public class FullGameInWindow {
+    public static void main(String[] args) {
+        new FullGameWindow();
+    }
+}
